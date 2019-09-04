0ef586f55119a875c6b1a0e17cb1a3b5d6db4def
federico <peteco1983@outlook.com>
2019-09-04 11:07:00 +0200

    Adding all exercises 2 and 3 in one file

--- RedCookies/index.js
+++ RedCookies/index.js
@@ -1,17 +1,39 @@
-//2.3
-function generateBandName(rawString) {
-  const both = rawString[0].toUpperCase() + rawString.substring(1).toLowerCase();
-  return `${both} `;
+//2.1
+const myArray = [];
+
+function generateBandName(clothingColor, lastFoodEaten) {
+  let bandName = ''
+  let color = clothingColor[0].toUpperCase();
+  color += clothingColor.substring(1).toLowerCase();
+
+  let food = lastFoodEaten[0].toUpperCase();
+  food += lastFoodEaten.substring(1).toLowerCase()
+
+  bandName = `The ${color} ${food}`
+
+  myArray.push(bandName);
+
+  return bandName;
 }
 
-console.log(generateBandName("crimson") + generateBandName("KEBAB"));
+console.log(generateBandName("brown", "beans"));
+generateBandName("rosa", "spagheTTi")
+console.log(myArray)
+
+// //2.3
+// function generateBandName(rawString) {
+//   const both = rawString[0].toUpperCase() + rawString.substring(1).toLowerCase();
+//   return `${both} `;
+// }
+
+// console.log(generateBandName("crimson") + generateBandName("KEBAB"));
 
 
-// 2.4 
+// // 2.4 
 
 
-function makeTasty(color)
-let tastyColor = ''
-switch (color) {
+// function makeTasty(color)
+// let tastyColor = ''
+// switch (color) {
 
-}
+// }
