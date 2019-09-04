38e0e8912d7f5764f3e187844d2a2693c7887999
federico <peteco1983@outlook.com>
2019-09-04 10:08:58 +0200

    WIP

--- RedCookies/index.js
+++ RedCookies/index.js
@@ -1,6 +1,17 @@
+//2.3
 function generateBandName(rawString) {
   const both = rawString[0].toUpperCase() + rawString.substring(1).toLowerCase();
   return `${both} `;
 }
 
 console.log(generateBandName("crimson") + generateBandName("KEBAB"));
+
+
+// 2.4 
+
+
+function makeTasty(color)
+let tastyColor = ''
+switch (color) {
+
+}
