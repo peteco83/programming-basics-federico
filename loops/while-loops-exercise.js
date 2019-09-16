986ae8f6e750c85168bcc27ac4370c9e479153a5
peteco83 <peteco1983@outlook.com>
2019-09-16 20:39:58 +0200

    Add exercise while loop 2

--- loops/while-loops-exercise.js
+++ loops/while-loops-exercise.js
@@ -1,6 +1,20 @@
-let looping = 0;
+// While loops 1.
 
-while (looping < 15) {
-  console.log(`It will run until ${looping}`);
-  looping += 1;
+// let looping = 0;
+
+// while (looping < 15) {
+//   console.log(`It will run until ${looping}`);
+//   looping += 1;
+// }
+
+// While loops 2.
+
+let myArrayOfCities = ["Buenos Aires", "Mexico DF", "Paris", "Madrid"];
+let cityString = "";
+let x = 0;
+
+while (x < myArrayOfCities.length) {
+  cityString += myArrayOfCities[x];
+  ++x;
 }
+console.log(myArrayOfCities);
