593a10dd1363bb298ddf3983d0cdfb13b89a24c4
federico <peteco1983@outlook.com>
2019-09-17 13:19:34 +0200

    Add do while Ex 3

--- loops/do-while-exercise.js
+++ loops/do-while-exercise.js
@@ -1,23 +1,38 @@
 // Do while Loops 1.
-// let arr = [1, 2, 3];
-// let index = 0;
-// let result = 0;
+let arr = [1, 2, 3];
+let index = 0;
+let result = 0;
 
-// console.log(`Before: ${arr}`);
-// do {
-//   result += arr[index];
-//   ++index;
-// } while (index < arr.length);
-// console.log(`After: ${result}`);
+console.log(`Before: ${arr}`);
+do {
+  result += arr[index];
+  ++index;
+} while (index < arr.length);
+console.log(`After: ${result}`);
 
 // Do while loops 2.
 
-let number = 1;
-let result = 1;
+// let number = 1;
+// let result = 1;
 
-do {
-  result = (number * (number + 1)) / 2;
-  ++number;
-} while (number <= 20);
+// do {
+//   result = (number * (number + 1)) / 2;
+//   ++number;
+// } while (number <= 20);
 
-console.log(`The result is ${result}`);
+// console.log(`The result is ${result}`);
+
+// Do while loop 3
+
+// let myArray = ["matt", "sara", "lara"];
+// let myIndex = 0;
+// let myCapitalizedArray = [];
+// console.log(`Names before: ${myArray}`);
+// do {
+//   myCapitalizedArray.push(
+//     myArray[myIndex][0].toUpperCase() + myArray[myIndex].substring(1)
+//   );
+//   ++myIndex;
+// } while (myIndex < myArray.length);
+
+// console.log(`Names after: ${myCapitalizedArray}`);
--- loops/while-loops-exercise.js
+++ loops/while-loops-exercise.js
@@ -9,37 +9,38 @@
 
 // While loops 2.
 
-// let myArrayOfCities = [
-//   "Buenos Aires ",
-//   "Mexico DF ",
-//   "Paris ",
-//   "Madrid ",
-//   "Berlin ",
-//   "Oslo"
-// ];
-// let cityString = "";
-// let x = 0;
+let myArrayOfCities = [
+  "Buenos Aires ",
+  "Mexico DF ",
+  "Paris ",
+  "Madrid ",
+  "Berlin ",
+  "Oslo"
+];
+let cityString = "";
+let x = 0;
 
-// while (x < myArrayOfCities.length) {
-//   cityString += myArrayOfCities[x];
-//   ++x;
-// }
-// console.log(cityString);
+while (x < myArrayOfCities.length) {
+  //   cityString += myArrayOfCities[x];
+  cityString = cityString + myArrayOfCities[x];
+  ++x;
+}
+console.log(cityString);
 
 // While loops 3.
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let index = 0;
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let index = 0;
 
-console.log(`Before: ${numbers}`);
-while (index < numbers.length) {
-  let isEven = numbers[index] % 2 === 0;
-  if (isEven) {
-    numbers[index] -= 1;
-  } else {
-    numbers[index] += 1;
-  }
-  ++index;
-}
+// console.log(`Before: ${numbers}`);
+// while (index < numbers.length) {
+//   let isEven = numbers[index] % 2 === 0;
+//   if (isEven) {
+//     numbers[index] -= 1;
+//   } else {
+//     numbers[index] += 1;
+//   }
+//   ++index;
+// }
 
-console.log(`After:  ${numbers}`);
+// console.log(`After:  ${numbers}`);
