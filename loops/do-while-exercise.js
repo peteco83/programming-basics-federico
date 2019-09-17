36f9e71458590b7deb79d8b54ed76480060a1b32
federico <peteco1983@outlook.com>
2019-09-17 11:30:39 +0200

    add 2 exer of do while loop

--- loops/do-while-exercise.js
+++ loops/do-while-exercise.js
@@ -1,11 +1,23 @@
 // Do while Loops 1.
-let arr = [1, 2, 3];
-let index = 0;
-let result = 0;
+// let arr = [1, 2, 3];
+// let index = 0;
+// let result = 0;
 
-console.log(`Before: ${arr}`);
+// console.log(`Before: ${arr}`);
+// do {
+//   result += arr[index];
+//   ++index;
+// } while (index < arr.length);
+// console.log(`After: ${result}`);
+
+// Do while loops 2.
+
+let number = 1;
+let result = 1;
+
 do {
-  result += arr[index];
-  ++index;
-} while (index < arr.length);
-console.log(`After: ${result}`);
+  result = (number * (number + 1)) / 2;
+  ++number;
+} while (number <= 20);
+
+console.log(`The result is ${result}`);
