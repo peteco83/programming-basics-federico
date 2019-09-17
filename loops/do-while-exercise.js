af9a68c9de151db5ee684cdb26317cf6179b2748
federico <peteco1983@outlook.com>
2019-09-17 14:13:03 +0200

    Add ex 5 do while loop

--- loops/do-while-exercise.js
+++ loops/do-while-exercise.js
@@ -1,38 +1,55 @@
-// Do while Loops 1.
-let arr = [1, 2, 3];
+console.log("Do while Loops 1");
+let arr = [1, 2, 3, 4, 5, 20];
 let index = 0;
 let result = 0;
 
-console.log(`Before: ${arr}`);
+console.log(`\nBefore: ${arr}`);
 do {
   result += arr[index];
   ++index;
 } while (index < arr.length);
 console.log(`After: ${result}`);
 
-// Do while loops 2.
+console.log("\nDo while Loops 2");
 
-// let number = 1;
-// let result = 1;
+let number = 1;
+let resultTwo = 1;
 
-// do {
-//   result = (number * (number + 1)) / 2;
-//   ++number;
-// } while (number <= 20);
+do {
+  result = (number * (number + 1)) / 2;
+  ++number;
+} while (number <= 20);
 
-// console.log(`The result is ${result}`);
+console.log(`\nThe result is ${result}`);
 
-// Do while loop 3
+console.log("\nDo while Loops 3");
 
-// let myArray = ["matt", "sara", "lara"];
-// let myIndex = 0;
-// let myCapitalizedArray = [];
-// console.log(`Names before: ${myArray}`);
-// do {
-//   myCapitalizedArray.push(
-//     myArray[myIndex][0].toUpperCase() + myArray[myIndex].substring(1)
-//   );
-//   ++myIndex;
-// } while (myIndex < myArray.length);
+let myArray = ["matt", "sara", "lara"];
+let myIndex = 0;
+let myCapitalizedArray = [];
+console.log(`\nNames before: ${myArray}`);
+do {
+  myCapitalizedArray.push(
+    myArray[myIndex][0].toUpperCase() +
+      myArray[myIndex].toLowerCase().substring(1)
+  );
+  ++myIndex;
+} while (myIndex < myArray.length);
 
-// console.log(`Names after: ${myCapitalizedArray}`);
+console.log(`Names after: ${myCapitalizedArray}`);
+
+console.log("\nDo_while_Loops_5");
+
+let program = [];
+let i = 0;
+
+const repeatIt = (item, times) => {
+  do {
+    program.push(item);
+    ++i;
+  } while (i < times);
+
+  return program;
+};
+
+console.log(repeatIt("gorkeim", 5));
