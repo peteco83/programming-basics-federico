27a8866590b19190664fb90c42c81906cb52c2a7
federico <peteco1983@outlook.com>
2019-09-18 10:12:46 +0200

    Add second ex for loops

--- loops/for-loops-exercise.js
+++ loops/for-loops-exercise.js
@@ -1,4 +1,5 @@
 // Exercise 1
+console.log(`First exercise:\n`);
 const exercise1 = () => {
   let total = 0;
   for (let i = 1; i <= 20; i += 1) {
@@ -7,3 +8,13 @@ const exercise1 = () => {
   return total;
 };
 console.log(exercise1());
+
+// Exercise 2
+console.log(`\nSecond exercise:\n`);
+const exercise2 = () => {
+  let arr = ["one", "two", "three", "four", "five"];
+  for (let i = 0; i < 5; i += 1) {
+    console.log(`There are ${arr[i]} bottles of beer on the wall`);
+  }
+};
+exercise2();
