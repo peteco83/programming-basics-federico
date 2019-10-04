b7e328b7700e9cf53b895ed900d549a1996301b1
peteco83 <peteco1983@outlook.com>
2019-10-04 20:31:39 +0200

    Add tree exercise on nested loops file

--- javascript/loops/nested-loops.js
+++ javascript/loops/nested-loops.js
@@ -20,7 +20,7 @@
 
 // let dashPalo = "";
 
-// for (let i = 0; i < 3; i += 1) {
+// for (let i = 0; i < 5; i += 1) {
 //   dashPalo = "";
 //   if (i % 2 === 0) {
 //     for (let j = 0; j < 5; j += 1) {
@@ -56,31 +56,38 @@
 
 // exerciseEstrellas();
 
-<<<<<<< HEAD
-const triangulo = () => {
-  let estrellita = "";
-  for (let i = 0; i < 10; i += 1) {
-    for (let j = i; j => 0; j -= 1) {
-      triangulo.append(j + " ");
-=======
 // Challenge
 
+// const challenge = rows => {
+//     const rows = [];
+//     const stars = [' ', '*']
+
+// }
+
 const christmasTree = rows => {
-  for (let i = 0; i <= rows; i += 1) {
-    let estrellas = "";
-    for (let j = 0; j <= rows - i; j += 1) {
-      estrellas += " ";
+  let space1 = " ";
+
+  for (let i = 0; i < rows; i += 1) {
+    space1 = " ";
+    for (let j = rows; j > i; j -= 1) {
+      space1 += "   ";
     }
-    for (let k = 0; k < i; k += 1) {
-      estrellas += "*";
->>>>>>> 5732ba24986bf2bd514e8292d9fedb752560c35a
+    for (let k = 0; k <= i; k += 1) {
+      space1 += " * ";
     }
+    for (let j = 0; j < i; j += 1) {
+      space1 += " * ";
+    }
+    console.log(space1);
   }
-  console.log(estrellita);
+
+  // for (let i = 0; i < 2; i += 1) {
+  //   space2 += "   ";
+  // }
+  // for (let i = 0; i < 3; i += 1) {
+  //   space2 += " * ";
+  // }
+  // console.log(space2);
 };
 
-<<<<<<< HEAD
-triangulo();
-=======
-christmasTree(3);
->>>>>>> 5732ba24986bf2bd514e8292d9fedb752560c35a
+christmasTree(2);
